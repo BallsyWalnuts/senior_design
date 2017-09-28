f633b606bcceff98a3f9211a04f72fbf435f7414
Grant Richard Steiner <grantsteiner64@gmail.com>
2017-09-28 12:59:30 -0500

    organization

--- phone/Acc.py
+++ phone/Acc.py
@@ -9,7 +9,7 @@ import time
 new = [0, 0, 0]
 old = new
 droid = android.Android()
-endTime = 1
+endTime = 1000
 timeSensed = 0 
 i = 0
 droid.startSensingTimed(2, 10)
@@ -20,4 +20,5 @@ while timeSensed <= endTime:
      delta = [(new[0] - old[0]) * 360 / 20 // 1, (new[1] - old[1]) * 360 / 20 // 1, (new[2] - old[2]) * 360 / 20 // 1, i] 
      print (delta) 
      old = new
+     timeSensed += 10 
 droid.stopSensing()
