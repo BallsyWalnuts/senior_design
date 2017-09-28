6f180a059aa33fdd7b7b4fea55f714a84634eacf
Grant Richard Steiner <grantsteiner64@gmail.com>
2017-09-28 12:17:07 -0500

    formatting

--- Acc.py
+++ Acc.py
@@ -1,19 +1,23 @@
+#! /usr/bin/env python
+# Robert Seaton
+# 25 September 2017
+# SIUE
+
 import android
 import time
 
-
-new = [0,0,0]
-old=new
+new = [0, 0, 0]
+old = new
 droid = android.Android()
 endTime = 1
-timeSensed=0 
-i=0
-droid.startSensingTimed(2 ,10)
+timeSensed = 0 
+i = 0
+droid.startSensingTimed(2, 10)
 while timeSensed <= endTime: 
      time.sleep(.01)
-     i=1+i
-     new=droid.sensorsReadAccelerometer().result 
-     delta = [(new[0]-old[0])*360/20//1 ,(new[1]-old[1]) *360/20//1,(new[2]-old[2]) *360/20//1,i] 
+     i += 1
+     new = droid.sensorsReadAccelerometer().result 
+     delta = [(new[0] - old[0]) * 360 / 20 // 1, (new[1] - old[1]) * 360 / 20 // 1, (new[2] - old[2]) * 360 / 20 // 1, i] 
      print (delta) 
-     old=new
+     old = new
 droid.stopSensing()
