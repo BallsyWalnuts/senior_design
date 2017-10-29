93d03e825c26ab71d293a8ce1959f53b206278a4
Grant Richard Steiner <grantsteiner64@gmail.com>
2017-10-29 17:57:25 -0500

    misc fixes

--- phone/Acc.py
+++ phone/Acc.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-# Robert Seaton
+# Robert Seaton, Grant Steiner
 # 25 September 2017
 # Southern Illinois University Edwardsville
 
@@ -7,27 +7,31 @@ import android
 import time
 import socket
 
-new = [0, 0, 0]
-old = new
-droid = android.Android()
-endTime = 1000
-timeSensed = 0
-i = 0
-droid.startSensingTimed(2, 10)
-while timeSensed <= endTime:
-    time.sleep(.01)
-    i += 1
-    new = droid.sensorsReadAccelerometer().result
-    delta = [(new[0] - old[0]) * 360 / 20 // 1, (new[1] - old[1]) * 360 / 20 // 1, (new[2] - old[2]) * 360 / 20 // 1, i]
-    print (delta)
+UDP_IP = "120.0.0.1"  # IP address of the Raspberry Pi
+UDP_PORT = 8001  # Port that the Pi is listening on
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Initialize the socket
+
+
+def sock_send(data=None):
+    sock.sendto(data=data, flags=(UDP_IP, UDP_PORT))
+
+
+if __name__ == '__main__':
+    new = [0, 0, 0]
     old = new
-    timeSensed += 10
-droid.stopSensing()
-
-UDP_IP = "120.0.0.1"
-UDP_PORT = 8001
-MESSAGE = "Hello World"
-
-sock = socket.socket(socket.AF_INET,
-                     socket.SOCK_DGRAM)
-sock.sendto(data=MESSAGE, flags=(UDP_IP, UDP_PORT))
+    droid = android.Android()
+    endTime = 1000
+    timeSensed = 0
+    i = 0
+    droid.startSensingTimed(2, 10)
+    while timeSensed <= endTime:
+        time.sleep(.01)
+        i += 1
+        new = droid.sensorsReadAccelerometer().result
+        delta = [(new[0] - old[0]) * 360 / 20 // 1,
+                 (new[1] - old[1]) * 360 / 20 // 1]
+        sock_send(data=delta)  # send the data to the Pi
+        # print (delta)
+        old = new
+        timeSensed += 10
+    droid.stopSensing()
--- pi/sock_recv.py
+++ pi/sock_recv.py
@@ -12,7 +12,7 @@
 # http://web.archive.org/web/20131017130434/http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
 
 # NOT OPERATIONAL
-#TODO: add conditions so that daemon releases the bind on the port when asked to stop
+# TODO: add conditions so that daemon releases the bind on the port when asked to stop
 
 import socket
 import time
